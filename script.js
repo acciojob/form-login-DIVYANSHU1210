342b1442fb67d5f032fe4efa7ef63147f4836454
DIVYANSHU1210 <84267089+DIVYANSHU1210@users.noreply.github.com>
2023-09-22 23:33:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
-    //Write your code here
 
+
+	
+	var form = document.getElementById("form1");
+
+	var fname = form.elements["fname"].value;
+	var lname = form.elements["lname"].value;
+
+	console.log(fname, lname);
+	
 }
